e6d374e028f84f6d0098f4dda2a6927e8a63a9af
Andyhargrebil <Andyhargrebil@everestkc.net>
2018-02-18 08:39:09 -0600

    I hope this works

--- character Controllers/Assets/FirstPersonControls.cs
+++ character Controllers/Assets/FirstPersonControls.cs
@@ -12,11 +12,19 @@ public class FirstPersonControls : MonoBehaviour
     public Transform eyes;
     public Transform player;
     public Transform anchor;
+	public GameObject ammo;
+	public GameObject goal;
+
+	public GameObject[] needtoshoot;
+
+	public bool canMove = true;
 
     // Use this for initialization
     void Start()
     {
         new Vector3(1, 1, 1);
+
+		goal.SetActive (false);
     }
 
     // Update is called once per frame
@@ -24,44 +32,48 @@ public class FirstPersonControls : MonoBehaviour
     {
         Vector3 moveDirection = Vector3.zero;
 
+		needtoshoot = GameObject.FindGameObjectsWithTag("NeedtoDestroy");
+
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         float mouseW = Input.GetAxis("Mouse ScrollWheel");
 
-        if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.W) && canMove == true)
         {
             moveDirection += transform.forward;
         }
 
-        if (Input.GetKey(KeyCode.S))
+		if (Input.GetKey(KeyCode.S) && canMove == true)
         {
             moveDirection += -transform.forward;
         }
 
-        if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A) && canMove == true)
         {
-            moveDirection += -transform.right;
+			moveDirection += -transform.right;
         }
 
-        if (Input.GetKey(KeyCode.D))
+		if (Input.GetKey(KeyCode.D) && canMove == true)
         {
-            moveDirection += transform.right;
+			moveDirection += transform.right;
         }
 
         moveDirection.y = 0;
 
         if (moveDirection != Vector3.zero)
         {
-            player.forward = moveDirection;
+            //player.forward = moveDirection;
         }
 
-        if (mouseX > 0)
+		if (mouseX > 0 && canMove == true)
         {
             anchor.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
+			player.Rotate (Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
         }
-        if (mouseX < 0)
+		if (mouseX < 0 && canMove == true)
         {
             anchor.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime, Space.World);
+			player.Rotate (Vector3.up, -rotationSpeed * Time.deltaTime, Space.World);
         }
 
         float adjustedEuler = eyes.eulerAngles.x;
@@ -70,26 +82,40 @@ public class FirstPersonControls : MonoBehaviour
             adjustedEuler -= 360;
         }
 
-        if (mouseY > 0 && adjustedEuler > 5)
+		if (mouseY > 0 && adjustedEuler > 5 && canMove == true)
         {
             anchor.Rotate(Vector3.right, -rotationSpeed * Time.deltaTime);
         }
-        if (mouseY < 0 && adjustedEuler < 65)
+		if (mouseY < 0 && adjustedEuler < 65 && canMove == true)
         {
             anchor.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
         }
 
-        if (mouseW > 0)
+		if (mouseW > 0 && canMove == true)
         {
             eyes.position += eyes.forward;
         }
 
-        if (mouseW < 0)
+		if (mouseW < 0 && canMove == true)
         {
             eyes.position -= eyes.forward;
         }
 
+		if (Input.GetMouseButtonDown (0) && canMove == true)
+		{
+			Instantiate (ammo, player.position, player.rotation);
+		}
+
+		if (needtoshoot.Length == 0) {
+			goal.SetActive (true);
+		}
+
         player.position += moveDirection.normalized* speed * Time.deltaTime;
         transform.position = player.position;
     }
+
+	void OnTriggerEnter (Collider player)
+	{
+		canMove = false;
+	}
 }
