c70d0b83446e0c04b29d3054ec0d63b763b46f75
Andyhargrebil <Andyhargrebil@everestkc.net>
2018-02-18 08:04:45 -0600

    This should work now

---
+++ character controllers/Assets/Bulletmove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletmove : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision hits)
+	{
+		Destroy (gameObject);
+		if (hits.gameObject.tag == "NeedtoDestroy") {
+			Destroy (hits.gameObject);
+		}
+	}
+}
